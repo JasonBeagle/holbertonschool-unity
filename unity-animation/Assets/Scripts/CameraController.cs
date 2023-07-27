a7d71e267d61b55cf57000dae91af956e37ff8e0
Jason Beagle <skipskipperson385@gmail.com>
2023-07-27 14:03:58 -0500

    task3,4

--- unity-animation/Assets/Scripts/CameraController.cs
+++ unity-animation/Assets/Scripts/CameraController.cs
@@ -7,11 +7,8 @@ public class CameraController : MonoBehaviour
     private Vector3 offset;
     private GameObject player;
     public float turnSpeed = 4.0f;
-
-    // New variable to control if the camera should be inverted
     public bool isInverted;
 
-    // Start is called before the first frame update
     void Start()
     {
         Cursor.visible = false;
@@ -19,22 +16,24 @@ public class CameraController : MonoBehaviour
         player = GameObject.Find("Player");
         offset = transform.position - player.transform.position;
 
-         // Load the invert option
         if (PlayerPrefs.HasKey("InvertYToggle"))
         {
             isInverted = PlayerPrefs.GetInt("InvertYToggle") == 1;
         }
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
-        // Use the isInverted bool to determine whether to invert the Mouse Y input
         float mouseY = isInverted ? -Input.GetAxis("Mouse Y") : Input.GetAxis("Mouse Y");
 
+        // The Quaternion.AngleAxis methods rotate the offset based on the mouse inputs.
         offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * Quaternion.AngleAxis(mouseY * turnSpeed, Vector3.left) * offset;
+        
+        // We update the camera position based on the player position plus the offset.
         transform.position = player.transform.position + offset;
-        transform.LookAt(player.transform.position);
+        
+        // Instead of having the camera look at the player (which can cause rotation changes based on player rotation), we will make the camera look in the direction of the offset.
+        transform.rotation = Quaternion.LookRotation(-offset, Vector3.up);
     }
 
     public void UpdateInverted()
--- unity-animation/Assets/Scripts/PlayerController.cs
+++ unity-animation/Assets/Scripts/PlayerController.cs
@@ -4,74 +4,66 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10.0f; // Speed of the player
-    public float jumpForce = 5.0f; // Jump force
-    public float fallThreshold = -10.0f; // Threshold for considering the player falling
-    private bool isJumping = false; // Track if the player is currently jumping
+    public float speed = 10.0f;
+    public float jumpForce = 5.0f;
+    public float fallThreshold = -10.0f;
+    public float turnSpeed = 5.0f; // Speed of turning
+    private bool isJumping = false;
 
-    private CharacterController characterController; // The CharacterController component
-    private Vector3 startPosition; // The start position of the player
+    private CharacterController characterController;
+    private Vector3 startPosition;
 
-    private float verticalVelocity = 0.0f; // Vertical velocity for the jump
+    private float verticalVelocity = 0.0f;
 
     void Start()
     {
-        // Get the CharacterController component
         characterController = GetComponent<CharacterController>();
-
-        // Store the start position
         startPosition = transform.position;
     }
 
     void Update()
     {
-        // Check if the player has fallen off the platform
         if (transform.position.y < fallThreshold)
         {
-            // Move the player to the start position
             Vector3 newPosition = new Vector3(startPosition.x, startPosition.y + 10, startPosition.z);
-            characterController.enabled = false; // Disable the CharacterController temporarily
+            characterController.enabled = false;
             transform.position = newPosition;
-            characterController.enabled = true; // Enable the CharacterController again
-            // Reset the vertical velocity
+            characterController.enabled = true;
             verticalVelocity = 0.0f;
         }
 
-        // Get input from WASD keys
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        // Create a movement vector from the input, relative to the camera's orientation
         Vector3 forward = Camera.main.transform.forward;
         Vector3 right = Camera.main.transform.right;
 
-        // Ignore vertical (Y) component of the camera's forward and right vectors
         forward.y = 0;
         right.y = 0;
         forward.Normalize();
         right.Normalize();
 
         Vector3 movement = (forward * moveVertical + right * moveHorizontal) * speed * Time.deltaTime;
-
-        // Apply the movement
         characterController.Move(movement);
 
-        // If the space key is pressed and the player is not currently jumping
+        // Update character rotation to face direction of movement
+        if (movement != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, turnSpeed * Time.deltaTime);
+        }
+
         if (Input.GetButtonDown("Jump") && !isJumping)
         {
-            // Calculate the required jump velocity based on the desired jump height and gravity
             verticalVelocity = Mathf.Sqrt(2.0f * jumpForce * Mathf.Abs(Physics.gravity.y));
             isJumping = true;
         }
 
-        // Apply gravity
         verticalVelocity += Physics.gravity.y * Time.deltaTime;
 
-        // Apply vertical movement
         Vector3 verticalMovement = Vector3.up * verticalVelocity * Time.deltaTime;
         characterController.Move(verticalMovement);
 
-        // Detect if the character is on the ground
         if (characterController.isGrounded)
         {
             isJumping = false;
